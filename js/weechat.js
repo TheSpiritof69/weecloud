3adcd4cbdb72204f7f5f45f8de37ac280a4aa582
TheSpiritof69 <thespiritof69@gmail.com>
2014-10-18 11:51:31 +0200

    revert

--- js/weechat.js
+++ js/weechat.js
@@ -249,7 +249,7 @@
     WeeChatProtocol._getColorObj = function(str) {
         if (str.length === 2) {
             var code = parseInt(str);
-            if (code > 255) {
+            if (code > 16) {
                 // should never happen
                 return WeeChatProtocol._getDefaultColor();
             } else {
